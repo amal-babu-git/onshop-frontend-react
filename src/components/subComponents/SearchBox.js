b22e1ab74e3a6bd94dfdd82d7ced4124347569bf
Amal Babu <amalbabu1200@gmail.com>
2022-07-12 21:23:52 +0530

    search feature added

--- src/components/subComponents/SearchBox.js
+++ src/components/subComponents/SearchBox.js
@@ -1,22 +1,35 @@
 import { MDBBtn } from 'mdb-react-ui-kit';
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { fetchProducts } from '../../features/prodcuts/productSlice';
 
 
 const SearchBox = () => {
 
-    
+  const dispatch = useDispatch()
+
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const searchBoxHandler = (e) => {
+    e.preventDefault()
+    setSearchQuery(e.target.value)
+    console.log(searchQuery)
+    dispatch(fetchProducts({ page: `http://127.0.0.1:8000/store/products/?page=1&search=${searchQuery}` }))
+
+  }
 
   return (
     <div>
-     
+
       <form className="d-flex input-group w-auto">
         <input
           type="search"
           className="form-control"
           placeholder="Search"
           aria-label="Search"
+          onChange={searchBoxHandler}
         />
-        <MDBBtn rounded>Search</MDBBtn>
+        <MDBBtn rounded onClick={searchBoxHandler}>Search</MDBBtn>
       </form>
     </div>
   );
